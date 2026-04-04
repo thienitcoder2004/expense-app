bc896371db9354dc7917d50ec98dab79d73a04c0
Nguyễn Ngọc Thiện <162625134+thienitcoder2004@users.noreply.github.com>
2026-04-04 21:06:38 +0700

    reset css

--- vite.config.ts
+++ vite.config.ts
@@ -5,3 +5,9 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
 })
+
+module.exports = {
+  content: ["./src/**/*.{js,jsx}"],
+  theme: { extend: {} },
+  plugins: [],
+};
